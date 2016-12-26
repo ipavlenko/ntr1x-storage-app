3890fc703aaac7d12b85eb309dfeb2b4153f7283
Igor Pavlenko <ipavlenko@inbox.ru>
2016-12-26 22:50:55 +0500

    Initial commit

---
+++ src/main/java/com/ntr1x/storage/app/App.java
@@ -0,0 +1,18 @@
+package com.ntr1x.storage.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({
+	"com.ntr1x.storage",
+})
+public class App {
+
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/ntr1x/storage/app/JerseyConfig.java
@@ -0,0 +1,104 @@
+package com.ntr1x.storage.app;
+
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.message.filtering.EntityFilteringFeature;
+import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
+import org.glassfish.jersey.moxy.xml.MoxyXmlFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+import org.glassfish.jersey.server.spi.Container;
+import org.glassfish.jersey.server.spi.ContainerLifecycleListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.ntr1x.storage.core.converter.ConverterProvider;
+import com.ntr1x.storage.core.filtering.ResourceFilteringFeature;
+import com.ntr1x.storage.security.filters.AuthenticationFilter;
+import com.ntr1x.storage.security.filters.AuthorizationFilter;
+import com.ntr1x.storage.security.filters.CORSRequestFilter;
+import com.ntr1x.storage.security.filters.CORSResponseFilter;
+
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+
+@Component
+@Configuration
+public class JerseyConfig extends ResourceConfig {
+
+	protected ServiceLocator serviceLocator;
+	
+	@Value("${app.public.host}")
+    private String host;
+
+	@Value("${app.public.schemes}")
+    private String[] schemes;
+	
+	@Bean
+	@Scope("singleton")
+	public ServiceLocatorProvider getServiceLocator() {
+	    return new ServiceLocatorProvider(this);
+	}
+	
+    public JerseyConfig() {
+		
+		packages("com.ntr1x.storage");
+		
+		property(ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR, true);
+		
+		register(ApiListingResource.class);
+		register(SwaggerSerializers.class);
+		register(CORSRequestFilter.class);
+		register(CORSResponseFilter.class);
+		register(ConverterProvider.class);
+		register(MultiPartFeature.class);
+		register(MoxyXmlFeature.class);
+		register(MoxyJsonFeature.class);
+		register(EntityFilteringFeature.class);
+		register(ResourceFilteringFeature.class);
+		register(RolesAllowedDynamicFeature.class);
+		register(AuthenticationFilter.class);
+		register(AuthorizationFilter.class);
+		
+		register(new ContainerLifecycleListener() {
+		    
+            public void onStartup(Container container) {
+                serviceLocator = container.getApplicationHandler().getServiceLocator();
+            }
+
+            public void onReload(Container container) {
+                serviceLocator = container.getApplicationHandler().getServiceLocator();
+            }
+            public void onShutdown(Container container) {
+                serviceLocator = null;
+            }
+        });
+		
+		BeanConfig beanConfig = new BeanConfig();
+		beanConfig.setVersion("1.0.0");
+		beanConfig.setSchemes(schemes);
+		beanConfig.setHost(host);
+		beanConfig.setBasePath("");
+		beanConfig.setResourcePackage("com.ntr1x.storage");
+		beanConfig.setScan(true);
+	}
+    
+    public static class ServiceLocatorProvider {
+        
+        private JerseyConfig config;
+
+        private ServiceLocatorProvider(JerseyConfig config) {
+            this.config = config;
+        }
+        
+        public ServiceLocator get() {
+            
+            return config.serviceLocator;
+        }
+    }
+}
